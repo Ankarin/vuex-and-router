131f82914e799ff53b8ca69cc64d846dd6b6a557
Ankarin <ankarn41k@gmail.com>
2019-09-26 16:40:28 +0300

    fixed

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -6,14 +6,14 @@
     <li>Email: {{getUsers[this.$route.params.id-1].email}}</li>
     <li>Id: {{getUsers[this.$route.params.id-1].id}}</li>
     <li>Username: {{getUsers[this.$route.params.id-1].username}}</li>
-    <li>Phone: {{getUsers[this.$route.params.id-1].phne}}</li>
+    <li>Phone: {{getUsers[this.$route.params.id-1].phone}}</li>
     <li>Website: {{getUsers[this.$route.params.id-1].website}}</li>
     <li>Address: 
         <ul>
             <li>City: {{getUsers[this.$route.params.id-1].address.city}}</li>
             <li>Geo: {{getUsers[this.$route.params.id-1].address.geo.lat}} {{getUsers[this.$route.params.id-1].address.geo.lng}}</li>
             <li>Street: {{getUsers[this.$route.params.id-1].address.street}}</li>
-            <li>Suit: {{getUsers[this.$route.params.id-1].address.suit}}</li>
+            <li>Suit: {{getUsers[this.$route.params.id-1].address.suite}}</li>
             <li>Zipcode: {{getUsers[this.$route.params.id-1].address.zipcode}}</li>
         </ul>
     </li>
